7d051fc1ea75ece3dd04fd4cbae0ab0cedd24f5a
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-05-27 21:51:53 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Common/IDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.Common
+{
+    public interface IDbContext
+    {
+        public DbConnection Coonection { get; }
+    }
+}
---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Commom/DbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Tahaluf.PlusExam.Core.Common;
+
+namespace Tahaluf.PlusExam.Infra.Commom
+{
+    public class DbContext : IDbContext
+    {
+        private DbConnection _dbConnection;
+        private readonly IConfiguration _Configuration;
+
+        public DbContext(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public DbConnection Coonection
+        {
+            get
+            {
+                if (_dbConnection == null)
+                {
+                    //connect
+                    _dbConnection = new OracleConnection(_Configuration["ConnectionStrings:DBConnectionString"]);
+                    _dbConnection.Open();
+                }
+                else if (_dbConnection.State != ConnectionState.Open)
+                {
+                    _dbConnection.Open();
+                }
+
+                return _dbConnection;
+            }
+
+        }
+    }
+}
