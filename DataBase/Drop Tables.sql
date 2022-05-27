28df7c639d9336316be28d7b85efe710e8bd102a
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-05-27 19:47:24 +0300

    Add files via upload
    You can drop all tables using this order

---
+++ DataBase/Drop Tables.sql
@@ -0,0 +1,18 @@
+-- Start Code
+
+-- You can drop all tables using this order
+
+DROP TABLE Score;
+DROP TABLE PhoneNumber;
+DROP TABLE CreditCard;
+DROP TABLE Certificate;
+DROP TABLE Invoice;
+DROP TABLE Result;
+DROP TABLE Account;
+DROP TABLE CorrectAnswer;
+DROP TABLE QuestionOption;
+DROP TABLE Question;
+DROP TABLE Exam;
+DROP TABLE Course;
+
+-- End Code
