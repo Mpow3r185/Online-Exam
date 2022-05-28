2a3c23d0e9db6b0523bb762cc76caa1069175369
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-05-28 13:28:29 +0300

    Add files via upload
    Add Packages

---
+++ DataBase/Yazeed/Package For QuestionOption Table.sql
@@ -0,0 +1,73 @@
+--------------------------------------------------------
+-- Backage for Table QuestionOption
+--------------------------------------------------------
+
+/*
+*
+* * QuestionOption Package Header 
+*
+*/
+
+CREATE OR REPLACE PACKAGE QuestionOptionPackage AS
+    --Get All QuestionOption
+    PROCEDURE GetAllQuestionOption;
+	
+    --Update QuestionOption
+    PROCEDURE UpdateQuestionOption(
+			Oid Questionoption.id%TYPE, 
+			OContent Questionoption.optioncontent%TYPE, 
+			OQuestionId Questionoption.questionid%TYPE
+			);
+    
+	--Create QuestionOption
+    PROCEDURE CreateQuestionOption(
+			OContent Questionoption.optioncontent%TYPE, 
+			OQuestionId Questionoption.questionid%TYPE
+			);
+			
+    --Delete QuestionOption
+    PROCEDURE DeleteQuestionOption(Oid Questionoption.id%TYPE);
+END QuestionOptionPackage;
+
+
+
+
+/* 
+*
+* * QuestionOption Package Body
+*
+*/
+CREATE OR REPLACE PACKAGE Body QuestionOptionPackage AS
+    --Body For Get All QuestionOption
+    PROCEDURE GetAllQuestionOption AS O_all sys_refcursor;
+    BEGIN
+        open O_all for
+        select * from Questionoption;
+        DBMS_SQL.RETURN_RESULT(O_all);
+    END GetAllQuestionOption;
+
+    --Body For Update QuestionOption
+    PROCEDURE UpdateQuestionOption(Oid Questionoption.id%TYPE, OContent Questionoption.optioncontent%TYPE, OQuestionId Questionoption.questionid%TYPE) AS        
+    BEGIN
+    UPDATE Questionoption SET Optioncontent = OContent, Questionid=OQuestionId
+    WHERE Id = Oid;
+    COMMIT;
+    END UpdateQuestionOption;
+
+    --Body For Create QuestionOption
+    PROCEDURE CreateQuestionOption(OContent Questionoption.optioncontent%TYPE, OQuestionId Questionoption.questionid%TYPE) AS
+    BEGIN
+    INSERT INTO Questionoption(Optioncontent, Questionid)
+    VALUES (OContent , OQuestionId);
+    COMMIT;
+    END CreateQuestionOption;
+    
+    --Body For Delete QuestionOption
+    PROCEDURE DeleteQuestionOption(Oid Questionoption.id%TYPE) AS
+    BEGIN
+    DELETE Questionoption WHERE Id=Oid;
+    COMMIT;
+    END DeleteQuestionOption;
+    
+END QuestionOptionPackage;
+
