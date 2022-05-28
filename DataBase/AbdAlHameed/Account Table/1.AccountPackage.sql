81103a79bd2da9c17130eedc3dd53c652dcd80b6
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-05-28 10:32:59 +0300

    Update 1.AccountPackage.sql

--- DataBase/AbdAlHameed/Account Table/1.AccountPackage.sql
+++ DataBase/AbdAlHameed/Account Table/1.AccountPackage.sql
@@ -1,11 +1,9 @@
--- Start Code
-
-CREATE OR REPLACE PACKAGE AccountPackage AS
+create or replace PACKAGE AccountPackage AS
     
     -- CRUD Procedures
     -- Get Accounts Procedure
     PROCEDURE GetAccounts;
-    
+
     -- Create Account Procedure
     PROCEDURE CreateAccount(
         uName IN account.username%type,
@@ -17,7 +15,7 @@ CREATE OR REPLACE PACKAGE AccountPackage AS
         addr IN account.address%type,
         rName IN account.roleName%type,
         profileImg IN account.profilePicture%type);
-    
+
     -- Update Account Procedure
     PROCEDURE UpdateAccount(
         accid IN account.id%type,
@@ -30,11 +28,20 @@ CREATE OR REPLACE PACKAGE AccountPackage AS
         addr IN account.address%type,
         rName IN account.roleName%type,
         profileImg IN account.profilePicture%type);
-        
+
     -- Delete Account Procedure
     PROCEDURE DeleteAccount(accid IN account.id%type);
     -- CRUD Procedures
-    
+
+    -- Get Usernames Procedure
+    PROCEDURE GetUsernames;
+
+    -- Get Full Names Procedure
+    PROCEDURE GetFullNames;
+
+    -- Get Emails
+    PROCEDURE GetEmails;
+
     -- Get Accounts By UserName, Email, Full Name and Role Name Procedure
     PROCEDURE SearchAccount(
         uName IN account.username%type,
