36ac728046fdb3b281da3f1bcc6c4f333b2b4947
Mahmoud Hamarsheh <mahmoud7hamarsheh@gmail.com>
2022-05-28 16:02:33 +0300

    add1

--- Tahaluf.PlusExam/Tahaluf.PlusExam.API/Class.cs
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.API/Class.cs
@@ -4,11 +4,7 @@
     {
         public string Name { get; set; }
         
-        public string Name2 { get; set; }
         
-        public string Name3 { get; set; }
-
-        public string Name4 { get; set; }
        
     }
 }
