7dd5b691b2c3a1eaaa264d10b6ebe5251949f1dc
AbdAlHameedD <dabdalhammed6@gmail.com>
2022-05-28 14:08:16 +0300

    AbdAlHameed

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.API/Controllers/QuestionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tahaluf.PlusExam.API.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
