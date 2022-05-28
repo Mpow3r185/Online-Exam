716d3884bb8cc04b71ba79a19986850695ed47b7
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-05-28 21:54:37 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/ServiceInterface/IPhoneNumberService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.ServiceInterface
+{
+    public interface IPhoneNumberService
+    {
+        //Get All Phone Numbers
+        List<PhoneNumber> GetAll();
+        //Create
+        bool CreatePhoneNumber(PhoneNumber phoneNumber);
+        //Update
+        bool UpdatePhoneNumber(PhoneNumber phoneNumber);
+        //Delete
+        bool DeletePhoneNumber(int id);
+    }
+}
---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/ServiceInterface/IScoreService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.ServiceInterface
+{
+    public interface IScoreService
+    {
+        //Get All Scores
+        List<Score> GetAll();
+        //Create
+        bool CreateScore(Score score);
+        //Update
+        bool UpdateScore(Score score);
+        //Delete
+        bool DeleteScore(int id);
+    }
+}
