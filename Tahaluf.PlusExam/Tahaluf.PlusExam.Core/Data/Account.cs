a3acb30c3d0d54ec42a23fd40a9ac02360c2165e
AbdAlHameedD <dabdalhammed6@gmail.com>
2022-05-28 17:29:33 +0300

    1) Add Course, Exam and Account to Core/Data 2) Add Enums in Core/DTO

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Tahaluf.PlusExam.Core.DTO;
+
+namespace Tahaluf.PlusExam.Core.Data
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, NotNull, MinLength(5), MaxLength(12)]
+        public string Username { get; set; }
+
+        [Required, NotNull, MinLength(8), MaxLength(15)]
+        public string Password { get; set; }
+
+        [Required, NotNull ,MaxLength(320)]
+        public string Email { get; set; }
+
+        [Required, NotNull, MinLength(6), MaxLength(60)]
+        public string Fullname { get; set; }
+
+        [MinLength(3), MaxLength(6)]
+        public string Gender { get; set; }
+
+        public DateTime Bod { get; set; }
+
+        [MaxLength(255)]
+        public string Address { get; set; }
+
+        [MinLength(2), MaxLength(5)]
+        public AccountStatusOptions Status { get; set; }
+
+        [MinLength(5), MaxLength(7)]
+        public string Rolename { get; set; }
+
+        [MaxLength(255)]
+        public string ProfilePicture { get; set; }
+
+
+        //public ICollection<Result> Results { get; set; }
+        //public ICollection<Invoice> Invoices { get; set; }
+        //public ICollection<Certificate> Certificates { get; set; }
+        //public ICollection<Creditcard> Creditcards {get; set; }
+        //public ICollection<Phonenumber> Phonenumbers { get; set; }
+        //public ICollection<Score> Scores { get; set; }
+    }
+}
---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using static Tahaluf.PlusExam.Core.Data.Enums;
+
+namespace Tahaluf.PlusExam.Core.Data
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, NotNull, MaxLength(30)]
+        public string CourseName { get; set; }
+
+        [MaxLength(300)]
+        public string Description { get; set; }
+
+        [MaxLength(7)]
+        public StatusOptions Status { get; set; }
+
+        [MaxLength(255)]
+        public string CourseImage { get; set; }
+
+
+        public ICollection<Exam> Exams { get; set; }
+    }
+}
