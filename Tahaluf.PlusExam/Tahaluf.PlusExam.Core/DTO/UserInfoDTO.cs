5347f916dc6cfcd92180dc48857e96cb6f98d6b8
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-05-30 20:01:43 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/UserInfoDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class UserInfoDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
