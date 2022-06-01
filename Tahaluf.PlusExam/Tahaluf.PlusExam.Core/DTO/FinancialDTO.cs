fbe6cf0191a1541bb96c411108c5e6e0a5207e9f
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:23:55 +0300

    Add files via upload
    Add Financial DTO

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/FinancialDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class FinancialDTO
+    {
+        public int? Profit { get; set; }
+        public float? AVGOfProfit { get; set; }
+        public int? NumberOfInvoice { get; set; }
+    }
+}
