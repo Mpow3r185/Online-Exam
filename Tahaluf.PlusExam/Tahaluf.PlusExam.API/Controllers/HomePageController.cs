998a4395210e4292f71723a43f6d8a63bb4d9dc8
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:36:36 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.API/Controllers/HomePageController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomePageController : ControllerBase
+    {
+        #region Fields
+        private readonly IHomePageService homePageService;
+        #endregion Fields
+
+        #region Constructor
+        public HomePageController(IHomePageService _homePageService)
+        {
+            homePageService = _homePageService;
+        }
+        #endregion Constructor
+
+        #region CRUD_HomePage
+
+        [HttpGet]
+        public List<HomePage> GetHomePage()
+        {
+            return homePageService.GetHomePage();
+        }
+
+        [HttpPut]
+        public bool UpdateHomePage(HomePage homePage)
+        {
+            return homePageService.UpdateHomePage(homePage);
+        }
+
+        #endregion CRUD_HomePage
+    }
+}
