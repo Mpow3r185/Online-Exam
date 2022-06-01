49b3d0c9f630fb981804c314e579899f6dd0d8e9
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:32:29 +0300

    Add files via upload
    Add Home Page Service

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Service/HomePageService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.RepositoryInterface;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.Infra.Service
+{
+    public class HomePageService : IHomePageService
+    {
+        #region Fields
+        private readonly IHomePageRepository homePageRepository;
+        #endregion Fields
+
+        #region Constructor
+        public HomePageService(IHomePageRepository _homePageRepository)
+        {
+            homePageRepository = _homePageRepository;
+        }
+        #endregion Constructor
+
+        #region CRUD_HomePage
+
+        public List<HomePage> GetHomePage()
+        {
+            return homePageRepository.GetHomePage();
+        }
+
+        public bool UpdateHomePage(HomePage homePage)
+        {
+            return homePageRepository.UpdateHomePage(homePage);
+        }
+
+        #endregion CRUD_HomePage
+    }
+}
