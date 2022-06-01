0986849e442c3076feb7a643ec048aad98d4bc52
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:22:54 +0300

    Add files via upload
    Add Home Page Class

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/HomePage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.Data
+{
+    public class HomePage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SiteName { get; set; }
+        public string SiteEmail { get; set; }
+        public string SitePhoneNumber { get; set; }
+    }
+}
