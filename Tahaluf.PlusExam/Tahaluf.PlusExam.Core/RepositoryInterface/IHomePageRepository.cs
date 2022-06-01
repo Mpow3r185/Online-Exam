0410591d805c848df01b9acdc041e1d219f60004
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:25:37 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/RepositoryInterface/IHomePageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.RepositoryInterface
+{
+    public interface IHomePageRepository
+    {
+        // Get Accounts
+        List<HomePage> GetHomePage();
+
+        // Update HomePage
+        bool UpdateHomePage(HomePage homePage);
+    }
+}
