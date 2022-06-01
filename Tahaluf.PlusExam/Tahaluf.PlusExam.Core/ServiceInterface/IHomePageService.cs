79d4a961071725f23258db3035a87ad855f3cdfd
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-01 20:27:35 +0300

    Add files via upload
    Add IHomePage Service

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/ServiceInterface/IHomePageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.ServiceInterface
+{
+    public interface IHomePageService
+    {
+        //Get Home Page Info
+        List<HomePage> GetHomePage();
+
+        // Update HomePage
+        bool UpdateHomePage(HomePage homePage);
+    }
+}
