10375be133b666a585fcdc83e96fb0ffb04f564c
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-03 20:16:51 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/AllUsersDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class AllUsersDTO
+    {
+        public int TotalUsers { get; set; } 
+    }
+}
