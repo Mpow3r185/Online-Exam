cf691b2067e8b9d6bee52bf6668cb297149c8660
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-03 20:17:29 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/TotalCostDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class TotalCostDTO
+    {
+        public float TotalCost { get; set; } 
+    }
+}
