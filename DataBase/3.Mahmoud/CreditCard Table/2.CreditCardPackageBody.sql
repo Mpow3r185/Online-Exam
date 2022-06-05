a39a197f43495753b3ca4d2c767416223588d323
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-05 13:24:04 +0300

    Update 2.CreditCardPackageBody.sql

--- DataBase/3.Mahmoud/CreditCard Table/2.CreditCardPackageBody.sql
+++ DataBase/3.Mahmoud/CreditCard Table/2.CreditCardPackageBody.sql
@@ -1,58 +1,51 @@
-CREATE OR REPLACE PACKAGE BODY CreditCardPackage AS
+create or replace PACKAGE BODY CreditCardPackage AS
     
-    -- CRUD Procedures
-    -- Get CreditCards Procedure
-    PROCEDURE GetCreditCards AS
+    -- CRUD Procedures    
+    PROCEDURE CreditCardCRUD(
+        func IN VARCHAR DEFAULT NULL,
+        card_id IN creditcard.id%type DEFAULT NULL,
+        card_number IN Creditcard.cardnumber%type DEFAULT NULL,
+        amount IN Creditcard.balance%type DEFAULT NULL,
+        acc_id IN Creditcard.accountid%type DEFAULT NULL) AS
+        
         ref_cursor SYS_REFCURSOR;
     BEGIN
-        OPEN ref_cursor FOR
-        SELECT *
-        FROM CreditCard;
-        
-        DBMS_SQL.RETURN_RESULT(ref_cursor);
-    END GetCreditCards;
-    
-    
-    -- Create CreditCard Procedure
-    PROCEDURE CreateCreditCard(
-        card_number IN Creditcard.cardnumber%type,
-        amount IN Creditcard.balance%type,
-        acc_id IN Creditcard.accountid%type) AS
-        BEGIN
+    -- Create 
+        IF func = 'CREATE' THEN
             INSERT INTO CreditCard
             (cardnumber, balance, accountid)
             VALUES(card_number, amount, acc_id);
-        commit;
-        END CreateCreditCard;
-    
-    
-    -- Update CreditCard Procedure
-    PROCEDURE UpdateCreditCard(
-        card_id IN creditcard.id%type,
-        card_number IN Creditcard.cardnumber%type,
-        amount IN Creditcard.balance%type,
-        acc_id IN Creditcard.accountid%type) AS
-        BEGIN
+
+        COMMIT;
+    -- Update 
+    ELSIF func = 'UPDATE' THEN
             UPDATE Creditcard SET 
                 cardnumber = card_number,
                 balance = amount,
                 accountid = acc_id
-               
+
             WHERE id = card_id;
-            commit;
-        END UpdateCreditCard;
-        
-        
-    -- Delete CreditCard Procedure
-    PROCEDURE DeleteCreditCard(card_id IN CreditCard.id%type) AS
-    BEGIN
+
+            COMMIT;
+
+
+    -- Delete 
+    ELSIF func = 'DELETE' THEN
         DELETE from CreditCard WHERE id = card_id;
-        commit;
-    END DeleteCreditCard;
-    -- CRUD Procedures
-    
-    
-    -- Update Balance Procedure
+
+        COMMIT;
+    ELSE
+    -- Get
+        OPEN ref_cursor FOR
+        SELECT *
+        FROM CreditCard;
+
+        DBMS_SQL.RETURN_RESULT(ref_cursor);
+    END IF;
+END CreditCardCRUD;
+
+
+-- Update Balance Procedure
     PROCEDURE UpdateBalance(
         accid IN account.id%type,
         newBalance IN creditCard.balance%type) AS
@@ -60,8 +53,8 @@ CREATE OR REPLACE PACKAGE BODY CreditCardPackage AS
         UPDATE creditCard SET 
         balance = newBalance
         WHERE accountId = accid;
-        
+
         COMMIT;
     END UpdateBalance;
-    
+
 END CreditCardPackage;
