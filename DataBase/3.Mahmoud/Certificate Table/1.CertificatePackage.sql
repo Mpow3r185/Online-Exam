1124a43021252174015abe39d7c8b547734d51b2
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-05 13:21:11 +0300

    Update 1.CertificatePackage.sql

--- DataBase/3.Mahmoud/Certificate Table/1.CertificatePackage.sql
+++ DataBase/3.Mahmoud/Certificate Table/1.CertificatePackage.sql
@@ -1,24 +1,12 @@
-CREATE OR REPLACE PACKAGE CertificatePackage AS
+create or replace PACKAGE CertificatePackage AS
     
     -- CRUD Procedures
-    -- Get Certificates Procedure
-    PROCEDURE GetCertificates;
-    
-    -- Create Certificate Procedure
-    PROCEDURE CreateCertificate(
-        createDate IN Certificate.creatiodate%type,
-        exam_id IN Certificate.examid%type,
-        acc_id IN Certificate.accountid%type);
-    
-    -- Update Certificate Procedure
-    PROCEDURE UpdateCertificate(
-        CertificateID IN Certificate.ID%type,
-        createDate IN Certificate.creatiodate%type,
-        exam_id IN Certificate.examid%type,
-        acc_id IN Certificate.accountid%type);
-        
-    -- Delete Certificate Procedure
-    PROCEDURE DeleteCertificate(CertificateID IN Certificate.id%type);
+    PROCEDURE CertificateCRUD(
+        func IN VARCHAR DEFAULT NULL,
+        CertificateID IN Certificate.ID%type DEFAULT NULL,
+        createDate IN Certificate.creatiodate%type DEFAULT NULL,
+        exam_id IN Certificate.examid%type DEFAULT NULL,
+        acc_id IN Certificate.accountid%type DEFAULT NULL);
+
 
 END CertificatePackage;
-
