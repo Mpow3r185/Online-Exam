7981e04581c395db3b58df57bebffe3c186632b4
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-05 13:25:35 +0300

    Update 2.InvoicePackageBody.sql

--- DataBase/3.Mahmoud/Invoice Table/2.InvoicePackageBody.sql
+++ DataBase/3.Mahmoud/Invoice Table/2.InvoicePackageBody.sql
@@ -1,56 +1,49 @@
-
-
-CREATE OR REPLACE PACKAGE BODY InvoicePackage AS
+create or replace PACKAGE BODY InvoicePackage AS
     
-    -- CRUD Procedures
-    -- Get Invoices Procedure
-    PROCEDURE GetInvoices AS
+    -- CRUD Procedures    
+    PROCEDURE InvoiceCRUD(
+        func IN VARCHAR DEFAULT NULL,
+        invoiceID IN invoice.ID%type DEFAULT NULL,
+        createDate IN invoice.creationdate%type DEFAULT NULL,
+        exam_id IN invoice.examid%type DEFAULT NULL,
+        acc_id IN invoice.accountid%type DEFAULT NULL) AS
+        
         ref_cursor SYS_REFCURSOR;
     BEGIN
-        OPEN ref_cursor FOR
-        SELECT *
-        FROM Invoice;
-        
-        DBMS_SQL.RETURN_RESULT(ref_cursor);
-    END GetInvoices;
-    
-    -- Create Invoice Procedure
-    PROCEDURE CreateInvoice(
-        createDate IN invoice.creationdate%type,
-        exam_id IN invoice.examid%type,
-        acc_id IN invoice.accountid%type) AS
-        BEGIN
+    -- Create 
+        IF func = 'CREATE' THEN
             INSERT INTO Invoice
             (CreationDate, ExamId, AccountId)
             VALUES(createDate, exam_id, acc_id);
-        commit;
-        END CreateInvoice;
-    
-    -- Update Invoice Procedure
-    PROCEDURE UpdateInvoice(
-        invoiceID IN invoice.ID%type,
-        createDate IN invoice.creationdate%type,
-        exam_id IN invoice.examid%type,
-        acc_id IN invoice.accountid%type) AS
-        BEGIN
+
+        COMMIT;
+    -- Update 
+    ELSIF func = 'UPDATE' THEN
             UPDATE Invoice SET 
                 CreationDate = createDate,
                 examid = exam_id,
                 accountid = acc_id
-               
-            WHERE id = invoiceID;
-            commit;
-        END UpdateInvoice;
-        
-    -- Delete Invoice Procedure
-    PROCEDURE DeleteInvoice(invoiceID IN invoice.id%type) AS
-    BEGIN
-        DELETE from Invoice WHERE id = invoiceID;
-        commit;
-    END DeleteInvoice;
 
-     --Obtains Financial
-    PROCEDURE ObtainsFinancial AS O_all sys_refcursor;
+            WHERE id = invoiceID;
+
+            COMMIT;
+
+    -- Delete 
+    ELSIF func = 'DELETE' THEN
+        DELETE from Invoice WHERE id = invoiceID;
+
+        COMMIT;
+    ELSE
+    -- Get 
+        OPEN ref_cursor FOR
+        SELECT *
+        FROM Invoice;
+
+        DBMS_SQL.RETURN_RESULT(ref_cursor);
+    END IF;
+END InvoiceCRUD;
+
+PROCEDURE ObtainsFinancial AS O_all sys_refcursor;
     BEGIN
         open O_all for
         select SUM(E.cost) as Profit,CAST(AVG(E.cost) AS DECIMAL(10,2))  As AVGOfProfit,COUNT(I.id) as NumberOfInvoice 
@@ -59,5 +52,7 @@ CREATE OR REPLACE PACKAGE BODY InvoicePackage AS
         on I.examid = E.id;
         DBMS_SQL.RETURN_RESULT(O_all);
     END ObtainsFinancial;
+
+
 
 END InvoicePackage;
