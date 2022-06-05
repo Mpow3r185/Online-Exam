5b533225276395aeaf28c5c350762997863bd740
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-05 13:23:27 +0300

    Update 1.CreditCardPackage.sql

--- DataBase/3.Mahmoud/CreditCard Table/1.CreditCardPackage.sql
+++ DataBase/3.Mahmoud/CreditCard Table/1.CreditCardPackage.sql
@@ -1,30 +1,17 @@
-CREATE OR REPLACE PACKAGE CreditCardPackage AS
-    
-    -- CRUD Procedures
-    -- Get CreditCards Procedure
-    PROCEDURE GetCreditCards;
-    
-    -- Create CreditCard Procedure
-    PROCEDURE CreateCreditCard(
-        card_number IN Creditcard.cardnumber%type,
-        amount IN Creditcard.balance%type,
-        acc_id IN Creditcard.accountid%type);
-    
-    -- Update CreditCard Procedure
-    PROCEDURE UpdateCreditCard(
-        card_id IN creditcard.id%type,
-        card_number IN Creditcard.cardnumber%type,
-        amount IN Creditcard.balance%type,
-        acc_id IN Creditcard.accountid%type);
-        
-    -- Delete CreditCard Procedure
-    PROCEDURE DeleteCreditCard(card_id IN CreditCard.id%type);
-    -- CRUD Procedures
-    
-    -- Update Balance Procedure
+create or replace PACKAGE CreditCardPackage AS
+
+-- CRUD Procedure    
+    PROCEDURE CreditCardCRUD(
+        func IN VARCHAR DEFAULT NULL,
+        card_id IN creditcard.id%type DEFAULT NULL,
+        card_number IN Creditcard.cardnumber%type DEFAULT NULL,
+        amount IN Creditcard.balance%type DEFAULT NULL,
+        acc_id IN Creditcard.accountid%type DEFAULT NULL);
+
+
+-- Update Balance Procedure
     PROCEDURE UpdateBalance(
         accid IN account.id%type,
         newBalance IN creditCard.balance%type);
 
 END CreditCardPackage;
-
