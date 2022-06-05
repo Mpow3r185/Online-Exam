232c3daa33ecc5825af8fc9a63c9428143efc817
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-05 12:58:44 +0300

    Update 2.CorrectAnswerPackageBody.sql

--- DataBase/2.Haneen/CorrectAnswer Table/2.CorrectAnswerPackageBody.sql
+++ DataBase/2.Haneen/CorrectAnswer Table/2.CorrectAnswerPackageBody.sql
@@ -1,50 +1,42 @@
---   CORRECT ANSWER PACKAGE BODY
 
-CREATE OR REPLACE PACKAGE BODY CORRECTANSWERPACKAGE AS
-----------------------------------------------------
---  GET ALL
+--   Correct Answer PACKAGE BODY
 
-PROCEDURE GETALL AS
-C_ALL SYS_REFCURSOR;
-BEGIN
-OPEN C_ALL FOR
-SELECT * FROM CORRECTANSWER;
-DBMS_SQL.RETURN_RESULT(C_ALL);
-END GETALL;
-----------------------------------------------------
--- UPDATE CORRECT ANSWER
+CREATE OR REPLACE PACKAGE BODY CorrectAnswerPackage AS
 
-PROCEDURE UPDATECORRECTANSWER(
-    CANSWERID CORRECTANSWER.ID%TYPE,
-    QOID CORRECTANSWER.QUESTIONOPTIONID%TYPE
-    ) AS
-BEGIN
-UPDATE CORRECTANSWER SET
-    QUESTIONOPTIONID  = QOID
-    WHERE ID  = CANSWERID;
-COMMIT;
-END UPDATECORRECTANSWER;
-----------------------------------------------------
--- CREATE CORRECT ANSWER
+    -- Correct Answer CRUD Operations
+    PROCEDURE CorrectAnswerCRUD(
+        func IN VARCHAR DEFAULT NULL,
+        CANSWERID CORRECTANSWER.ID%TYPE DEFAULT NULL,
+        QOID CORRECTANSWER.QUESTIONOPTIONID%TYPE DEFAULT NULL)
+    AS
+        C_ALL SYS_REFCURSOR;
+    BEGIN
+        If func = 'CREATE' THEN
+            INSERT INTO CORRECTANSWER (QUESTIONOPTIONID)
+            VALUES (QOID);
+        
+            COMMIT; 
+        
+        ELSIF func = 'UPDATE' THEN
+            UPDATE CORRECTANSWER SET
+            QUESTIONOPTIONID  = QOID
+            WHERE ID  = CANSWERID;
+    
+            COMMIT;
+            
+        ELSIF func = 'DELETE' THEN
+            DELETE FROM CORRECTANSWER 
+            WHERE ID = CANSWERID;
+        
+            COMMIT;
+        
+        ELSE 
+            OPEN C_ALL FOR
+            SELECT * 
+            FROM CORRECTANSWER;
+        
+            DBMS_SQL.RETURN_RESULT(C_ALL);
+        END IF;        
+    END  CorrectAnswerCRUD;       
 
-PROCEDURE CREATECORRECTANSWER(
-        QOID CORRECTANSWER.QUESTIONOPTIONID%TYPE
-    ) AS
-BEGIN
-    INSERT INTO CORRECTANSWER (QUESTIONOPTIONID)
-    VALUES (QOID);
-COMMIT;
-END CREATECORRECTANSWER;
-----------------------------------------------------
--- DELETE CORRECT ANSWER
-
-PROCEDURE DELETECORRECTANSWER(CANSWERID CORRECTANSWER.ID%TYPE) AS
-BEGIN
-    DELETE FROM CORRECTANSWER 
-    WHERE ID = CANSWERID;
-COMMIT;
-END DELETECORRECTANSWER;
-------------------------------------------------------
-END CORRECTANSWERPACKAGE;
-
---******************************************************************************
+END CorrectAnswerPackage;
