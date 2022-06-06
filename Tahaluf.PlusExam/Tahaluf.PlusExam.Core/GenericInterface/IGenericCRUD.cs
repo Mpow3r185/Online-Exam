fd2c54af25caf17b9c8bb345145d4b0d8aab342b
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-06 10:27:35 +0300

    Update IGenericCRUD.cs

--- Tahaluf.PlusExam/Tahaluf.PlusExam.Core/GenericInterface/IGenericCRUD.cs
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/GenericInterface/IGenericCRUD.cs
@@ -6,7 +6,7 @@ namespace Tahaluf.PlusExam.Core.GenericInterface
 {
     public interface IGenericCRUD<T> where T : class
     {
-        List<T> GetAll();
+        dynamic GetAll();
         bool Create(T entity);
         bool Delete(int id);
         bool Update(T entity);
