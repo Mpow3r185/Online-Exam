db0305b178cfe66f4cda9a150a4a34e17208dc5e
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-06 10:45:35 +0300

    Update 2.QuestionOptionPackageBody.sql

--- DataBase/4.Yazeed/QuestionOption Table/2.QuestionOptionPackageBody.sql
+++ DataBase/4.Yazeed/QuestionOption Table/2.QuestionOptionPackageBody.sql
@@ -4,35 +4,36 @@
 *
 */
 CREATE OR REPLACE PACKAGE Body QuestionOptionPackage AS
-    --Body For Get All QuestionOption
-    PROCEDURE GetAllQuestionOption AS O_all sys_refcursor;
+    --Body For CRUD QuestionOption
+    PROCEDURE QuestionOptionCRUD(
+            func IN VARCHAR DEFAULT NULL,
+            Oid Questionoption.id%TYPE DEFAULT NULL, 
+			OContent Questionoption.optioncontent%TYPE DEFAULT NULL, 
+			OQuestionId Questionoption.questionid%TYPE DEFAULT NULL) AS O_all sys_refcursor;
     BEGIN
-        open O_all for
-        select * from Questionoption;
-        DBMS_SQL.RETURN_RESULT(O_all);
-    END GetAllQuestionOption;
-
-    --Body For Update QuestionOption
-    PROCEDURE UpdateQuestionOption(Oid Questionoption.id%TYPE, OContent Questionoption.optioncontent%TYPE, OQuestionId Questionoption.questionid%TYPE) AS        
-    BEGIN
-    UPDATE Questionoption SET Optioncontent = OContent, Questionid=OQuestionId
-    WHERE Id = Oid;
-    COMMIT;
-    END UpdateQuestionOption;
-
-    --Body For Create QuestionOption
-    PROCEDURE CreateQuestionOption(OContent Questionoption.optioncontent%TYPE, OQuestionId Questionoption.questionid%TYPE) AS
-    BEGIN
-    INSERT INTO Questionoption(Optioncontent, Questionid)
-    VALUES (OContent , OQuestionId);
-    COMMIT;
-    END CreateQuestionOption;
-    
-    --Body For Delete QuestionOption
-    PROCEDURE DeleteQuestionOption(Oid Questionoption.id%TYPE) AS
-    BEGIN
-    DELETE Questionoption WHERE Id=Oid;
-    COMMIT;
-    END DeleteQuestionOption;
+        -- Create
+        IF func = 'CREATE' THEN
+             INSERT INTO Questionoption(Optioncontent, Questionid)
+             VALUES (OContent , OQuestionId);
+             COMMIT;
+             
+         -- Update    
+        ELSIF func = 'UPDATE' THEN    
+             UPDATE Questionoption SET Optioncontent = OContent, Questionid=OQuestionId
+             WHERE Id = Oid;
+             COMMIT;
+        
+         --Delete    
+        ELSIF func = 'DELETE' THEN  
+             DELETE Questionoption WHERE Id=Oid;
+             COMMIT;
+        -- Get All 
+        ELSE    
+            open O_all for
+            select * from Questionoption;
+            DBMS_SQL.RETURN_RESULT(O_all);
+            
+       END IF;     
+    END QuestionOptionCRUD; 
     
 END QuestionOptionPackage;
