5211b77eb3db662d981612b58612eb4ba29bd755
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-06 10:38:47 +0300

    Update ResultRepository.cs

--- Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Repository/ResultRepository.cs
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Repository/ResultRepository.cs
@@ -13,13 +13,13 @@ namespace Tahaluf.PlusExam.Infra.Repository
     public class ResultRepository : IResultRepository
     {
         #region Fields
-        private readonly IDbContext _dbContext;
+         private readonly IGenericCRUD<Result> genericCRUD;
         #endregion Fields
 
         #region Constructor
         public ResultRepository(IDbContext DbContext)
         {
-            _dbContext = DbContext;
+            genericCRUD = new GenericCRUD<Result>(DbContext);
         }
         #endregion Constructor
 
@@ -28,79 +28,28 @@ namespace Tahaluf.PlusExam.Infra.Repository
         #region CreateResult
         public bool CreateResult(Result result)
         {
-            #region DynamicParameters
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("RQuestionOptionId",
-                result.QuestionOptionId, 
-                dbType: DbType.Int32, 
-                direction: ParameterDirection.Input);
-
-            parameters.Add("RAccountId",
-                result.AccountId, 
-                dbType: DbType.Int32, 
-                direction: ParameterDirection.Input);
-            #endregion DynamicParameters
-
-            _dbContext.Connection.ExecuteAsync(
-                "ResultPackage.CreateResult", parameters,
-                commandType: CommandType.StoredProcedure);
-            
-            return true;
+           return genericCRUD.Create(result);
         }
         #endregion CreateResult
 
         #region DeleteResult
         public bool DeleteResult(int id)
         {
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("Rid",
-                id,
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-
-            _dbContext.Connection.ExecuteAsync(
-                "ResultPackage.DeleteResult", parameters,
-                commandType: CommandType.StoredProcedure);
-            
-            return true;
+           return genericCRUD.Delete(id);
         }
         #endregion DeleteResult
 
         #region GetResults
         public List<Result> GetResults()
         {
-            return _dbContext.Connection.Query<Result>(
-                "ResultPackage.GetAllResult", 
-                commandType: CommandType.StoredProcedure).ToList();
+           return genericCRUD.GetAll();
         }
         #endregion GetResults
 
         #region UpdateResult
         public bool UpdateResult(Result result)
         {
-            #region DynamicParameters
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("Rid",
-                result.AccountId,
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-
-            parameters.Add("RQuestionOptionId",
-                result.QuestionOptionId,
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-
-            parameters.Add("RAccountId",
-                result.AccountId, 
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-            #endregion DynamicParameters
-
-            _dbContext.Connection.ExecuteAsync(
-                "ResultPackage.UpdateResult", parameters,
-                commandType: CommandType.StoredProcedure);
-            
-            return true;
+            return genericCRUD.Update(result);
         }
         #endregion UpdateResult
 
