3b20c650a03a452d2fcb31bb445549b9b9bfcf87
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-06 20:21:11 +0300

    Update CreditCardRepository.cs

--- Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Repository/CreditCardRepository.cs
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Repository/CreditCardRepository.cs
@@ -1,4 +1,4 @@
-﻿using Dapper;
+using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Text;
 using Tahaluf.PlusExam.Core.Common;
 using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.GenericInterface;
 using Tahaluf.PlusExam.Core.RepositoryInterface;
+using Tahaluf.PlusExam.Infra.Generic;
 
 namespace Tahaluf.PlusExam.Infra.Repository
 {
@@ -14,11 +16,13 @@ namespace Tahaluf.PlusExam.Infra.Repository
     {
         #region Fields
         private readonly IDbContext dbContext;
+        private readonly IGenericCRUD<CreditCard> genericCRUD;
         #endregion Fields
 
         #region Constructor
         public CreditCardRepository(IDbContext _dbContext)
         {
+            genericCRUD = new GenericCRUD<CreditCard>(_dbContext);
             dbContext = _dbContext;
         }
         #endregion Constructor
@@ -28,92 +32,53 @@ namespace Tahaluf.PlusExam.Infra.Repository
         #region GetCreditCards
         public List<CreditCard> GetCreditCards()
         {
-            return dbContext.Connection.Query<CreditCard>(
-                "CreditCardPackage.GetCreditCards", 
-                commandType: CommandType.StoredProcedure).ToList();
+            return genericCRUD.GetAll();
         }
         #endregion GetCreditCards
 
         #region CreateCreditCard
         public bool CreateCreditCard(CreditCard creditCard)
         {
-            #region DynamicParameters
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("card_number",
-                creditCard.CardNumber,
-                dbType: DbType.Int64,
-                direction: ParameterDirection.Input);
 
-            parameters.Add("amount",
-                creditCard.Balance,
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-
-            parameters.Add("acc_id",
-                creditCard.AccountId,
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-            #endregion DynamicParameters
-
-            dbContext.Connection.ExecuteAsync(
-                "CreditCardPackage.CreateCreditCard", parameters, 
-                commandType: CommandType.StoredProcedure);
-
-            return true;
+            return genericCRUD.Create(creditCard);
         }
         #endregion CreateCreditCard
 
         #region UpdateCreditCard
         public bool UpdateCreditCard(CreditCard creditCard)
         {
-            #region DynamicParameters
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("card_id", 
-                creditCard.Id,
-                dbType: DbType.Int32, 
-                direction: ParameterDirection.Input);
-
-            parameters.Add("card_number",
-                creditCard.CardNumber,
-                dbType: DbType.Int64,
-                direction: ParameterDirection.Input);
-
-            parameters.Add("amount",
-                creditCard.Balance, 
-                dbType: DbType.Int32,
-                direction: ParameterDirection.Input);
-
-            parameters.Add("acc_id", 
-                creditCard.AccountId, 
-                dbType: DbType.Int32, 
-                direction: ParameterDirection.Input);
-            #endregion DynamicParameters
-
-            dbContext.Connection.ExecuteAsync(
-                "CreditCardPackage.UpdateCreditCard", parameters,
-                commandType: CommandType.StoredProcedure);
-
-            return true;
+            return genericCRUD.Update(creditCard);
         }
         #endregion UpdateCreditCard
 
         #region DeleteCreditCard
         public bool DeleteCreditCard(int id)
         {
+            return genericCRUD.Delete(id);
+        }
+
+        #endregion DeleteCreditCard
+
+
+        #endregion CRUD_Operation
+        public bool UpdateBalance(int id, double newBalance)
+        {
             DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("card_id",
-                id, 
+            parameters.Add("accid",
+                id,
+                dbType: DbType.Int32,
+                direction: ParameterDirection.Input);
+
+            parameters.Add("newBalance",
+                id,
                 dbType: DbType.Int32,
                 direction: ParameterDirection.Input);
 
             dbContext.Connection.ExecuteAsync(
-                "CreditCardPackage.DeleteCreditCard", parameters, 
+                "CreditCardPackage.UpdateBalance", parameters,
                 commandType: CommandType.StoredProcedure);
 
             return true;
         }
-        #endregion DeleteCreditCard
-
-        #endregion CRUD_Operation
     }
 }
