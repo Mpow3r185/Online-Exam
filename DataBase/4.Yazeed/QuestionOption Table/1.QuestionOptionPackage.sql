da0143cddeba3cc1aa991791905989eda42341a3
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-06 10:44:42 +0300

    Update 1.QuestionOptionPackage.sql

--- DataBase/4.Yazeed/QuestionOption Table/1.QuestionOptionPackage.sql
+++ DataBase/4.Yazeed/QuestionOption Table/1.QuestionOptionPackage.sql
@@ -9,22 +9,10 @@
 */
 
 CREATE OR REPLACE PACKAGE QuestionOptionPackage AS
-    --Get All QuestionOption
-    PROCEDURE GetAllQuestionOption;
-	
-    --Update QuestionOption
-    PROCEDURE UpdateQuestionOption(
-			Oid Questionoption.id%TYPE, 
-			OContent Questionoption.optioncontent%TYPE, 
-			OQuestionId Questionoption.questionid%TYPE
-			);
-    
-	--Create QuestionOption
-    PROCEDURE CreateQuestionOption(
-			OContent Questionoption.optioncontent%TYPE, 
-			OQuestionId Questionoption.questionid%TYPE
-			);
-			
-    --Delete QuestionOption
-    PROCEDURE DeleteQuestionOption(Oid Questionoption.id%TYPE);
+    --QuestionOption CRUD operation
+    PROCEDURE QuestionOptionCRUD(
+            func IN VARCHAR DEFAULT NULL,
+            Oid Questionoption.id%TYPE DEFAULT NULL, 
+	    OContent Questionoption.optioncontent%TYPE DEFAULT NULL, 
+	    OQuestionId Questionoption.questionid%TYPE DEFAULT NULL);
 END QuestionOptionPackage;
