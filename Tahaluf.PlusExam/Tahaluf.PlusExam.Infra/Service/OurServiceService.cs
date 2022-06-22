1cc69e37d5c5d23b7369e01c6c675cee789fe3a7
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-22 13:57:03 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Service/OurServiceService.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.RepositoryInterface;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.Infra.Service
+{
+    public class OurServiceService : IOurServiceService
+    {
+
+        #region Fields
+        private readonly IOurServiceRepository _ourServiceRepository;
+        #endregion Fields
+
+        #region Constructor
+        public OurServiceService(IOurServiceRepository ourServiceRepository)
+        {
+            _ourServiceRepository = ourServiceRepository;
+        }
+        #endregion Constructor
+
+        #region CRUD_Operation
+        public bool CreateOurService(OurService ourService)
+        {
+            return _ourServiceRepository.CreateOurService(ourService);
+        }
+
+        public bool DeleteOurService(int id)
+        {
+            return _ourServiceRepository.DeleteOurService(id);
+        }
+
+        public List<OurService> GetOurService()
+        {
+            return _ourServiceRepository.GetOurService();
+        }
+
+        public bool UpdateOurService(OurService ourService)
+        {
+            return _ourServiceRepository.UpdateOurService(ourService);
+        }
+        #endregion CRUD_Operation
+    }
+}
