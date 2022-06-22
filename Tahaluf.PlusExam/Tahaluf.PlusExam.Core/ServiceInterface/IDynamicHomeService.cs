90735321a29e07cba09ac01c28f62879b98d44dc
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-22 17:59:08 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/ServiceInterface/IDynamicHomeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.ServiceInterface
+{
+    public interface IDynamicHomeService
+    {
+        // Get All
+        List<DynamicHome> GetAll();
+
+        // Update Home
+        bool UpdateHome(DynamicHome dynamicHome);
+    }
+}
