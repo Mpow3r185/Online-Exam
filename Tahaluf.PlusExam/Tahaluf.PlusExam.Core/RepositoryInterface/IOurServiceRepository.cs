c8e327ef99340473f9c2264a30a84b656e7a2546
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-22 13:58:19 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/RepositoryInterface/IOurServiceRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.RepositoryInterface
+{
+    public interface IOurServiceRepository
+    {
+        // Get Service
+        List<OurService> GetOurService();
+
+        // Create Service
+        bool CreateOurService(OurService ourService);
+
+        // Update Service
+        bool UpdateOurService(OurService ourService);
+
+        // Delete Service
+        bool DeleteOurService(int id);
+    }
+}
