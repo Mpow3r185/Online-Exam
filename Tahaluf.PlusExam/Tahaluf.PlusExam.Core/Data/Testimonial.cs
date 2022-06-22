b257310e9914cd3ba1d2f90309d83c87b4f5239e
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-22 11:56:06 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/Testimonial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.Data
+{
+    public class Testimonial
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, NotNull,MaxLength(255)]
+        public string Message { get; set; }
+
+        [MaxLength(8)]
+        public string Status { get; set; }
+        public int AccountId { get; set; }
+
+        [ForeignKey("AccountId")]
+        public virtual Account Account { get; set; }
+
+    }
+}
