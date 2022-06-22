a02aebdc25cbb3667132c652558797e882f8180b
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-22 13:57:44 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/OurService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.Data
+{
+    public class OurService
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
