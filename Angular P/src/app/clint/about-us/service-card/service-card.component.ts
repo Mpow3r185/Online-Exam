a6692bf79e26c5cb7366c4eacc483877315fd531
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-22 14:06:42 +0300

    Update service-card.component.ts

--- Angular P/src/app/clint/about-us/service-card/service-card.component.ts
+++ Angular P/src/app/clint/about-us/service-card/service-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public home: HomeService) { }
 
   ngOnInit(): void {
   }
