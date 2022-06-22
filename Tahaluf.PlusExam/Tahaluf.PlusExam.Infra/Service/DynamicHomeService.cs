5c98bcf1f521f36b0e9cd6e6e792fe697e449f70
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-22 18:01:11 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Service/DynamicHomeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.RepositoryInterface;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.Infra.Service
+{
+    public class DynamicHomeService : IDynamicHomeService
+    {
+        #region Fields
+        private readonly IDynamicHomeRepository dynamicHomeRepository;
+        #endregion Fields
+
+        #region Constructor
+        public DynamicHomeService(IDynamicHomeRepository _dynamicHomeRepository)
+        {
+            dynamicHomeRepository = _dynamicHomeRepository;
+        }
+        #endregion Constructor
+
+        public List<DynamicHome> GetAll()
+        {
+            return dynamicHomeRepository.GetAll();
+        }
+
+        public bool UpdateHome(DynamicHome dynamicHome)
+        {
+            return dynamicHomeRepository.UpdateHome(dynamicHome);
+        }
+        
+    }
+}
