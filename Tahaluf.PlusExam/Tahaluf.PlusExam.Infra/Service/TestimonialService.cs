fea563ef3c611fba48f587e53b3da1d7d862646c
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-22 12:01:26 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Infra/Service/TestimonialService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.RepositoryInterface;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.Infra.Service
+{
+    public class TestimonialService : ITestimonialService
+    {
+        #region Fields
+        private readonly ITestimonialRepository testimonialRepository;
+        #endregion Fields
+
+        #region Constructor
+        public TestimonialService(ITestimonialRepository _testimonialRepository)
+        {
+            testimonialRepository = _testimonialRepository;
+        }
+        #endregion Constructor
+
+        #region GetTestimonials
+        public List<Testimonial> GetTestimonials()
+        {
+            return testimonialRepository.GetTestimonials();
+        }
+        #endregion GetTestimonials
+
+        #region CreateTestimonial
+        public bool CreateTestimonial(Testimonial testimonial)
+        {
+            return testimonialRepository.CreateTestimonial(testimonial);
+        }
+        #endregion CreateTestimonial
+
+        #region DeleteTestimonial
+        public bool DeleteTestimonial(int id)
+        {
+            return testimonialRepository.DeleteTestimonial(id);
+        }
+        #endregion DeleteTestimonial
+
+    }
+}
