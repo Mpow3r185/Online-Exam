852bc361fb30d328d536a57b3fe061c448d4b7d8
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-22 17:58:29 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/RepositoryInterface/IDynamicHomeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.RepositoryInterface
+{
+    public interface IDynamicHomeRepository
+    {
+        // Get All
+        List<DynamicHome> GetAll();
+
+        // Update Home
+        bool UpdateHome(DynamicHome dynamicHome);
+    }
+}
