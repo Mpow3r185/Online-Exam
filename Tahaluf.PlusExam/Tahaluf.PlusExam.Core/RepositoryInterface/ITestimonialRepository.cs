d3e7cf56cc69a9d184c54a7eb602150392178d85
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-22 11:58:43 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/RepositoryInterface/ITestimonialRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.PlusExam.Core.Data;
+
+namespace Tahaluf.PlusExam.Core.RepositoryInterface
+{
+    public interface ITestimonialRepository
+    {
+        // Get Testimonials
+        List<Testimonial> GetTestimonials();
+
+
+        // Create Testimonial
+        bool CreateTestimonial(Testimonial testimonial);
+
+
+        // Delete Testimonial
+        bool DeleteTestimonial(int id);
+    }
+}
