a70fddb68e6d1c097276cee3e587c46cd6f87c20
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-22 17:48:14 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.API/Controllers/DynamicHomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.API.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class DynamicHomeController : ControllerBase
+        {
+            #region Fields
+            private readonly IDynamicHomeService dynamicHomeService;
+            #endregion Fields
+
+            #region Constructor
+            public DynamicHomeController(IDynamicHomeService _IdynamicHomeService)
+            {
+                dynamicHomeService = _IdynamicHomeService;
+            }
+            #endregion Constructor
+
+            #region CRUD_HomePage
+
+            [HttpGet]
+            public List<DynamicHome> GetHomePage()
+            {
+                return dynamicHomeService.GetAll();
+            }
+
+            [HttpPut]
+            public bool UpdateHomePage(DynamicHome dynamicHome)
+            {
+                return dynamicHomeService.UpdateHome(dynamicHome);
+            }
+
+            #endregion CRUD_HomePage
+        }
+    }
