9a781ad0945c7b59a394cf4cd2988d57f2c769aa
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-22 12:02:21 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.API/Controllers/TestimonialController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Tahaluf.PlusExam.Core.Data;
+using Tahaluf.PlusExam.Core.ServiceInterface;
+
+namespace Tahaluf.PlusExam.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        #region Fields
+        private readonly ITestimonialService testimonialService;
+        #endregion Fields
+
+        #region Constructor
+        public TestimonialController(ITestimonialService _testimonialService)
+        {
+            testimonialService = _testimonialService;
+        }
+        #endregion Constructor
+
+        #region CreateTestimonial
+        [HttpPost]
+        public bool CreateTestimonial(Testimonial testimonial)
+        {
+            return testimonialService.CreateTestimonial(testimonial);
+        }
+        #endregion CreateTestimonial
+
+        #region DeleteTestimonial
+        [HttpDelete]
+        [Route("deleteTestimonial/{id}")]
+        public bool DeleteTestimonial(int id)
+        {
+            return testimonialService.DeleteTestimonial(id);
+        }
+        #endregion DeleteTestimonial
+
+        #region GetTestimonials
+        [HttpGet]
+        public List<Testimonial> GetTestimonials()
+        {
+            return testimonialService.GetTestimonials();
+        }
+        #endregion GetTestimonials
+    }
+}
