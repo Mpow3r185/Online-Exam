8b589711b3ab241c6ae73344bc8afeb0cbf6549d
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-23 12:22:16 +0300

    Update registration.component.ts

--- Angular P/src/app/authentication/registration/registration.component.ts
+++ Angular P/src/app/authentication/registration/registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthenticationService } from 'src/app/service/authentication.service';
+import { SpinnerComponent } from '../../spinner/spinner.component';
 
 @Component({
   selector: 'app-registration',
@@ -10,15 +13,28 @@ import { AuthenticationService } from 'src/app/service/authentication.service';
 export class RegistrationComponent implements OnInit {
   hide = true;
   response!: { dbPath: "" };
-  constructor(public authService: AuthenticationService, private router: Router) { }
+
+  constructor(public authService: AuthenticationService, private router: Router,private toastr: ToastrService) { }
+
+   //For Register new user
+   registerForm: FormGroup = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(60)]),
+    username: new FormControl('', [Validators.required,Validators.minLength(5), Validators.maxLength(15)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(8),Validators.maxLength(15)]),
+  });
+
+
+
   ngOnInit(): void {
   }
 
   onPasswordChange() {
-    if (this.authService.registerForm.controls.password.value == this.authService.registerForm.controls.confirmPassword.value) {
-      this.authService.registerForm.controls.confirmPassword.setErrors(null);
+    if (this.registerForm.controls.password.value == this.registerForm.controls.confirmPassword.value) {
+      this.registerForm.controls.confirmPassword.setErrors(null);
     } else {
-      this.authService.registerForm.controls.confirmPassword.setErrors({ mismatch: true });
+      this.registerForm.controls.confirmPassword.setErrors({ mismatch: true });
     }
   }
 
@@ -26,16 +42,17 @@ export class RegistrationComponent implements OnInit {
     this.response = event;
   };
 
-  createCustomer() {
-    if (this.response.dbPath != null || this.response.dbPath != '') {
-      this.authService.registerForm.controls.profilePic.setValue(this.response.dbPath);
+  createUser() {
+    
+    if(this.registerForm.valid){
+      this.authService.createNewUser(this.registerForm.value);
     }
-    else
-    {
-      this.authService.registerForm.controls.profilePic.setErrors({required: true});
+    else {
+        this.toastr.error('You Must Fill The Fields First')
     }
-    this.authService.createNewCustomer();
   }
+
+
   goToLogin(){
     this.router.navigate(['auth/login'])
   }
