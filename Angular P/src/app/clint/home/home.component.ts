cadc16414ac45ff03dfada0ed5dc5961d006e8cc
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-06-25 00:32:55 +0300

    Update home.component.ts

--- Angular P/src/app/clint/home/home.component.ts
+++ Angular P/src/app/clint/home/home.component.ts
@@ -8,7 +8,6 @@ import SwiperCore, {
 } from 'swiper';
 import { HomeService } from 'src/app/service/home.service';
 import { MatDialog } from '@angular/material/dialog';
-import { SpinnerComponent } from 'src/app/spinner/spinner.component';
 import { UserService } from 'src/app/service/user.service';
 import { Router } from '@angular/router';
 
@@ -26,36 +25,10 @@ SwiperCore.use([EffectCoverflow, Pagination]);
 export class HomeComponent implements OnInit {
   loopStatues: boolean = false;
   search!: string;
-  adData: any = [];
+  searchExams: any = [];
+  searchCourses: any = [];
   accountStatus: boolean = false;
   testimonialTextarea = new FormControl('', [Validators.required, Validators.minLength(10)]);
-
-
-// Test Data 
-  courses:any=[{
-    courseName:'React',
-    courseImage:'assets/images/reactcourse.png'
-  },
-  {
-    courseName:'Angular',
-    courseImage:'assets/images/angular.png'
-  },
-  {
-    courseName:'Data Base',
-    courseImage:'assets/images/dbcourse.jpg'
-  }];
-
-  sliderImages: any = [
-    {
-      imagePath: 'slider-1.jpg'
-    },
-    {
-      imagePath: 'slider-2.jpg'
-    },
-    {
-      imagePath: 'slider-3.jpg'
-    }
-  ];
 
     testimonials: any = [
     {
@@ -115,19 +88,11 @@ export class HomeComponent implements OnInit {
     public userService: UserService,
     private router: Router
   ) {
-    
   }
 
-  people = [{
-    name: {
-      firstName: 'Jesse',
-      lastName: 'Bowen',
-    },
-    state: 'Seattle',
-  }];
-
   ngOnInit(): void {
-
+    this.homeService.getCourses();
+    this.homeService.getExams();
   }
 
   currentTestimonialIndex: number = 0;
@@ -137,5 +102,46 @@ export class HomeComponent implements OnInit {
     document.getElementById('testiSlider')!.style.transform = `translateX(${index * -1110}px)`;
     document.getElementById(`transporter${index}`)!.classList.add('disable-div');
     this.currentTestimonialIndex = index;
+  }
+
+  homeSearch() {
+    this.searchCourses = [];
+    this.searchExams = [];
+    const searchContainer = document.getElementById('searchContainer');
+
+    if (this.search) {
+      searchContainer!.style.display = 'block';
+      const coursesContainer = document.getElementById('coursesContainer');
+      const examsContainer = document.getElementById('examsContainer');
+      
+      for (let obj of this.homeService.courses) {
+        let courseName: string = obj.courseName.toLowerCase();
+        if (courseName.includes(this.search.toLowerCase())) {
+          this.searchCourses.push(obj);
+        } 
+      }
+      
+      for (let obj of this.homeService.exams) {
+        let title: string = obj.title.toLowerCase();
+        if (title.includes(this.search.toLowerCase())) {
+          this.searchExams.push(obj);
+        }
+      }
+            
+      if (this.searchCourses.length < 1) {
+        coursesContainer!.style.display = 'none';
+      } else {
+        coursesContainer!.style.display = 'flex';
+      }
+
+      if (this.searchExams.length < 1) {
+        examsContainer!.style.display = 'none';
+      } else {
+        examsContainer!.style.display = 'flex';
+      }
+    }
+    else {
+      searchContainer!.style.display = 'none';
+    }
   }
 }
