4f142ef0370b49b02ed296064e038afd039de202
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-24 18:55:13 +0300

    update navbar ts

--- Angular P/src/app/internal-nav-bar/internal-nav-bar.component.ts
+++ Angular P/src/app/internal-nav-bar/internal-nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from '../service/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { HomeService } from '../service/home.service';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-internal-nav-bar',
@@ -12,15 +14,23 @@ export class InternalNavBarComponent implements OnInit {
   isScrolled: boolean = false;
   accountStatus: boolean = false;
   
-  constructor(public userService: UserService, private router: Router) {    }
+  constructor(public homeService: HomeService, private router: Router,  private toastr: ToastrService) {    }
 
   async ngOnInit() {
     if (localStorage.getItem('token')) {
+      this.homeService.getUserByUserName();
       this.accountStatus = true;
     }
   }
 
   logOut() {
+    SpinnerComponent.show();
+    setTimeout(() => {
+      SpinnerComponent.hide();
+    }, 2000);
+    setTimeout(() => {
+      this.toastr.success("Logout Successfully");
+    }, 2000);
     localStorage.clear();
     this.router.navigate(['/auth/login']);
   }
