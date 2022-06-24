98473c8d48da4e62ec6d92876256bac10a5cce1b
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-06-25 00:47:57 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/PopularCoursesDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class PopularCoursesDTO
+    {
+        public int CourseId { get; set; }
+        public int Summation { get; set; }
+    }
+}
