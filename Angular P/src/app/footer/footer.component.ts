87846cf0bec652916b9933675fa98598a6852440
Mahmoud Hamarsheh <63678521+Mpow3r185@users.noreply.github.com>
2022-06-26 00:26:00 +0300

    Update footer.component.ts

--- Angular P/src/app/footer/footer.component.ts
+++ Angular P/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../service/home.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public homeService: HomeService) { }
 
   ngOnInit(): void {
+    this.homeService.getHomePage();
   }
 
 }
