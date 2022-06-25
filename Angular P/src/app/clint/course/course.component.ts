cd980d474978e865ed3b157a531aec2e3cca3943
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-06-25 23:30:09 +0300

    Update course.component.ts

--- Angular P/src/app/clint/course/course.component.ts
+++ Angular P/src/app/clint/course/course.component.ts
@@ -23,35 +23,8 @@ export class CourseComponent implements OnInit {
     public userService: UserService,
     private router: Router) { }
 
-  // Test Data
-  courses:any=[{
-    courseName:'React',
-    courseImage:'assets/images/reactcourse.png'
-  },
-  {
-    courseName:'Angular',
-    courseImage:'assets/images/angular.png'
-  },
-  {
-    courseName:'Data Base',
-    courseImage:'assets/images/dbcourse.jpg'
-  },
-  {
-    courseName:'Web Api',
-    courseImage:'assets/images/apicourse.png'
-  },
-  {
-    courseName:'C#',
-    courseImage:'assets/images/c.jpg'
-  },
-  {
-    courseName:'Entrepreneurship',
-    courseImage:'assets/images/entrep.jpeg'
-  }];
-
-  ngOnInit(): void {
-    SpinnerComponent.show();
-    setTimeout(() => SpinnerComponent.hide(), 2000);
+  async ngOnInit(): Promise<void> {
+    await this.homeService.getCourses();
   }
 }
 
