6ce42788c28deb5bdf4403a871222f1b5240c6f6
Abd Al Hameed Dalgamouni <82041118+AbdAlHameedD@users.noreply.github.com>
2022-06-26 21:34:16 +0300

    Update home.component.ts

--- Angular P/src/app/clint/home/home.component.ts
+++ Angular P/src/app/clint/home/home.component.ts
@@ -93,7 +93,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.homeService.getCourses();
     this.homeService.getExams();
-    this.homeService.getHomePage();
+    //this.homeService.getHomePage();
   }
 
   currentTestimonialIndex: number = 0;
