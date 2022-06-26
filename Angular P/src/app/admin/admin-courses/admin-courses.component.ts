d96992623ed3322aad33682bab51cf2b6b8345dc
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-26 23:33:25 +0300

    Add files via upload

---
+++ Angular P/src/app/admin/admin-courses/admin-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-courses',
+  templateUrl: './admin-courses.component.html',
+  styleUrls: ['./admin-courses.component.css']
+})
+export class AdminCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
