08da461748b64f5755236ff92ff512614ebba6c6
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-27 16:25:16 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/TestimonialDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class TestimonialDTO
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string UserName { get; set; }   
+        public string FullName { get; set; }   
+
+        public string Message { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Status { get; set; }
+    }
+}
