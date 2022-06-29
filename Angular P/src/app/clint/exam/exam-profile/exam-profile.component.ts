d84b1c46d09561e2e9cab82a926cd3a942ae7bbd
AbdAlHameedD <dabdalhammed6@gmail.com>
2022-06-29 10:07:23 +0300

    Fixes Some Issues

--- Angular P/src/app/clint/exam/exam-profile/exam-profile.component.ts
+++ Angular P/src/app/clint/exam/exam-profile/exam-profile.component.ts
@@ -12,8 +12,8 @@ import { HomeService } from 'src/app/service/home.service';
 export class ExamProfileComponent implements OnInit {
 
   isExpanded: boolean = true;
-  timerStatus: any;
-  timerExam: any;
+  timerExam!: string;
+  timerStatus: string | undefined;
   private routeSub!: Subscription;
 
   constructor(public homeService: HomeService, private route: ActivatedRoute) {}
@@ -26,7 +26,45 @@ export class ExamProfileComponent implements OnInit {
     });
     await delay(1000);
 
+    this.examTimer();
     this.getExamDuration();
+  }
+
+  async examTimer() {
+    await delay(1000);
+
+    let startTime = new Date(this.homeService.exams.startDate).getTime();
+    let endTime = new Date(this.homeService.exams.endDate).getTime();
+    let nowTime: number = new Date().getTime();
+
+    if (startTime < nowTime && endTime < nowTime) {
+      this.timerStatus = undefined;
+      this.timerExam = 'EXPIRED';      
+    } else if (startTime < nowTime && endTime > nowTime) {
+      this.timerStatus = "ENDS AFTER";
+      this.timerFunc(endTime - nowTime);
+    } else {
+      this.timerStatus = "START AFTER";
+      this.timerFunc(startTime - nowTime);
+    }    
+  }
+  
+  timerFunc(distance: number) {
+    let timer = setInterval(() => {
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      hours += days * 24;
+      this.timerExam = hours + ":" + minutes + ":" + seconds;
+
+      if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+        clearInterval(timer);
+        this.examTimer();
+      }
+      distance -= 1000;
+    }, 1000);
   }
 
   expandExamInformation(): void {
