d8f9bfe8ee6017a057dd80f20ee5fff3c6cd051a
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-06-29 22:00:27 +0300

    Update admin.service.ts

--- Angular P/src/app/service/admin.service.ts
+++ Angular P/src/app/service/admin.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { HomeService } from './home.service';
+import { SpinnerComponent } from 'src/app/spinner/spinner.component';
 
 @Injectable({
   providedIn: 'root'
