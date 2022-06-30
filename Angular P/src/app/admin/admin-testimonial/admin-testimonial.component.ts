75b45b65664bce87d6ba189cb84a609de5899122
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-06-30 21:22:50 +0300

    Update admin-testimonial.component.ts

--- Angular P/src/app/admin/admin-testimonial/admin-testimonial.component.ts
+++ Angular P/src/app/admin/admin-testimonial/admin-testimonial.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 import { AdminService } from 'src/app/service/admin.service';
+import { SpinnerComponent } from 'src/app/spinner/spinner.component';
 declare const $:any
-
 @Component({
   selector: 'app-admin-testimonial',
   templateUrl: './admin-testimonial.component.html',
@@ -9,9 +11,9 @@ declare const $:any
 })
 export class AdminTestimonialComponent implements OnInit {
 
- 
-  constructor(public adminService:AdminService ) { }
-  
+  @ViewChild('callDeleteDialog') callDeleteDialog!: TemplateRef<any>;
+  @ViewChild('callDeleteDialog2') callDeleteDialog2!: TemplateRef<any>;
+  constructor(public adminService:AdminService,private dialog: MatDialog,private toastr:ToastrService ) { }
    ngAfterViewInit(): void {
     setTimeout(() => {
       $('#dtTable4').DataTable({
@@ -30,19 +32,87 @@ export class AdminTestimonialComponent implements OnInit {
        status:"ACCEPTED"
       }
       
-      this.adminService.testimonials=this.adminService.testimonials.filter((data:any) => data.id!=a.id)
+    //  this.adminService.testimonials=this.adminService.testimonials.filter((data:any) => data.id!=a.id)
       this.adminService.updateTestimonial(body);
+      this.toastr.success('Accepted');
     }
     console.log(a);
      
   }
-  deletetest(id:number)
-  {
-    this.adminService.testimonials=this.adminService.testimonials.filter((data:any) => data.id!=id)
-    this.adminService.DeleteTestimonial(id);
+  handleRejected(a:any){
+    if(a.status=="PENDING" || a.status=="ACCEPTED")
+    {
+      let body:any={
+       
+       id:a.id,
+       status:"REJECTED"
+      }
+      
+    //  this.adminService.testimonials=this.adminService.testimonials.filter((data:any) => data.id!=a.id)
+      this.adminService.updateTestimonial(body);
+      this.toastr.success('Rejected');
+    }
+    console.log(a);
+     
+  }
+
+  // deletetest(id:number)
+  // {
+  //   this.adminService.testimonials=this.adminService.testimonials.filter((data:any) => data.id!=id)
+  //   this.adminService.DeleteTestimonial(id);
+  // }
+
+  //This For Delete Testimonial Dialog
+  
+   openDeleteDialog(id: number) {
+
+    const dialogRef = this.dialog.open(this.callDeleteDialog);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result != undefined) {
+          if (result == 'yes') {
+            this.adminService.DeleteTestimonial(id);
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000); 
+            
+          }
+      }
+    })
+  }
+
+
+   //This For Delete Testimonial Dialog
+  
+   openAcceptRejectDialog(obj:any) {
+
+    const dialogRef = this.dialog.open(this.callDeleteDialog2);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result != undefined) {
+          if (result == 'yes') {
+            this.handleAccepted(obj);
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000); 
+            
+          }
+          else{
+            this.handleRejected(obj)
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+          }
+      }
+    })
   }
   ngOnInit(): void {
+    SpinnerComponent.show();
     this.adminService.getTestimonials();
+    setTimeout(() => {
+      SpinnerComponent.hide();
+    }, 3500);
   }
 
 }
+
