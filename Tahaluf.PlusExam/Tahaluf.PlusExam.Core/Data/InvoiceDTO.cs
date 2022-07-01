28b84c59751e2ff69ee4b20349ddcea9fd5cd7d2
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-07-01 21:46:42 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/InvoiceDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class InvoiceDTO
+    {
+        public DateTime creationDate { get; set; }
+        public string examName { get; set; }
+        public string examLevel { get; set; }
+        public decimal examCost { get; set; }
+    }
+}
