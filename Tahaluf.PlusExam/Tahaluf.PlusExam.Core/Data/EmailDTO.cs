dcd54bb71f8b673cbf940defcc94ea48d8c7e5f1
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-07-01 15:51:52 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/Data/EmailDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class EmailDTO
+    {
+        public int studentId { get; set; }
+        public string SendTo { get; set; }
+        public string SendFrom { get; set; }
+        public string emailPassword { get; set; }
+        public string examPassword { get; set; }
+    }
+}
