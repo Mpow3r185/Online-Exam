0e0de7b09ad7f22d5adfbce8a5f42e22a6e1cb7c
HaneenMomani <105963882+HaneenMomani@users.noreply.github.com>
2022-07-03 21:00:04 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/ReportDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class ReportDTO
+    {
+        public int AccId { get; set; }
+        public string CourseName { get; set; }
+        public string Title { get; set; }
+        public float Cost { get; set; }
+        public string ExamLevel { get; set; }
+        public DateTime CreatioDate { get; set; }   
+        public float Grade { get; set; } 
+        public string FullName { get; set; }
+    }
+}
