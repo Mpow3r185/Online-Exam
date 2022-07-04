a6f3d98c7b953b43e36def29b468274aff0974bb
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-07-04 21:49:15 +0300

    Update user-profile-routing.module.ts

--- Angular P/src/app/user-profile/user-profile-routing.module.ts
+++ Angular P/src/app/user-profile/user-profile-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { UserProfileModule } from './user-profile.module';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 const routes: Routes = [
   {
