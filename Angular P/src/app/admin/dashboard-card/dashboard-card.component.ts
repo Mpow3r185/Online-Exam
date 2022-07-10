61d780d6457eb712b123f39818d103d80dbc7b32
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-07-10 19:02:45 +0300

    Update dashboard-card.component.ts

--- Angular P/src/app/admin/dashboard-card/dashboard-card.component.ts
+++ Angular P/src/app/admin/dashboard-card/dashboard-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { CalendarOptions } from '@fullcalendar/angular';
+import { AdminService } from 'src/app/service/admin.service';
 import { HomeService } from 'src/app/service/home.service';
-import { UserService } from 'src/app/service/user.service';
+import { SpinnerComponent } from 'src/app/spinner/spinner.component';
 
 @Component({
   selector: 'app-dashboard-card',
@@ -10,9 +11,40 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class DashboardCardComponent implements OnInit {
   numOfUser!: number;
-  numOfAdvertisement!: number;
-  constructor(public homeService: HomeService, public dialog: MatDialog, public userService: UserService) { }
+  numOfCourse!: number;
+
+  lastFiveUser:any;
+  lastFiveCourse:any;
+
+  constructor(public homeService: HomeService, public adminService: AdminService) { }
 
   ngOnInit(): void {
+    SpinnerComponent.show();
+    this.adminService.getAllCourses();
+    this.adminService.getAllAccounts();
+
+    setTimeout(() => {
+      this.numOfUser = this.adminService.AccountsData.length;
+      this.numOfCourse = this.adminService.CoursesData.length;
+
+      this.lastFiveUser = this.adminService.AccountsData.slice(Math.max(this.numOfUser - 5, 1));
+      this.lastFiveCourse = this.adminService.CoursesData.slice(Math.max(this.numOfCourse - 5, 1));
+
+    }, 2000);
+
+    setTimeout(() => {
+      SpinnerComponent.hide();
+    }, 3500);
+
+    
+    
   }
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+  };
+
+ 
+
+
 }
