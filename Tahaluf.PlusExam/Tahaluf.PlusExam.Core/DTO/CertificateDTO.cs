80029fd8ef7bf25f7e2e9e0bb625584da46b9535
YazeedBaniAta <83374942+YazeedBaniAta@users.noreply.github.com>
2022-07-10 17:57:28 +0300

    Add files via upload

---
+++ Tahaluf.PlusExam/Tahaluf.PlusExam.Core/DTO/CertificateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.PlusExam.Core.DTO
+{
+    public class CertificateDTO
+    {
+        public DateTime creationDate { get; set; }
+        public string examName { get; set; }
+        public string examLevel { get; set; }
+        public DateTime examDate { get; set; }
+    }
+}
